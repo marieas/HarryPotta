cb3eeade992bc65161294f31243f883878e93004
marie <marie@getacademy.no>
2023-12-14 14:06:00 +0100

    update

---
+++ HarryPottah/Letter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HarryPottah
+{
+    internal class Letter
+    {
+        public string Message { get; set; }
+        public Character Recipient { get; set; }
+        public Letter(string msg, Character character)
+        {
+            Message = msg;
+            Recipient = character;
+        }
+    }
+}
